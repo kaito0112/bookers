b560e305f4d66d29b08574c5829cabe656291ffe
kaito0112 <123792716+kaito0112@users.noreply.github.com>
2023-02-05 06:07:44 +0000

    bookers-13

--- db/migrate/20230205055027_add_body_to_books.rb
+++ db/migrate/20230205055027_add_body_to_books.rb
@@ -1,4 +1,4 @@
-class AddBodyToBook < ActiveRecord::Migration[6.1]
+class AddBodyToBooks < ActiveRecord::Migration[6.1]
   def change
     add_column :books, :body, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_02_004735) do
+ActiveRecord::Schema.define(version: 2023_02_05_055027) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
